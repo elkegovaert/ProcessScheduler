6daad5bce7eac3bae9bc25e2e176c183a1664d87
elkegovaert <elke.govaert@outlook.com>
2020-03-10 18:08:39 +0100

    FCFS klasse gemaakt

---
+++ src/FirstComeFirstServed.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+public class FirstComeFirstServed {
+
+    public FirstComeFirstServed() {}
+
+    //First Come First Served Scheduling Algorithme
+    static public List<Process> FCFS(List<Process> processList) {
+
+        //Process 1 wordt steeds als eerste uitgevoerd
+        Process process1 = processList.get(0);
+        int serviceTime1=process1.getServiceTime();
+        int endTime1=process1.getServiceTime()+process1.getArrivalTime();
+        process1.setEndTime(endTime1);
+        process1.setWaitTime(0);
+        process1.setStartTime(process1.getArrivalTime());
+        process1.setTAT(0+serviceTime1);
+        process1.setNormTAT(process1.getTAT()/serviceTime1);
+
+        for(int i=1;i<processList.size();i++) {
+
+            Process processVorige = processList.get(i-1);
+            Process process=processList.get(i);
+            int arrivalTime = process.getArrivalTime();
+            int serviceTime = process.getServiceTime();
+            int waitTime = processVorige.getEndTime()-arrivalTime;
+            if(waitTime<0)waitTime=0;
+            int startTime;
+            if(processVorige.getEndTime()<arrivalTime) {
+                startTime=arrivalTime;
+            }
+            else {
+                startTime=processVorige.getEndTime();
+            }
+            int endTime = startTime+serviceTime;
+            int TAT = serviceTime + waitTime;
+            double normTAT = (double)TAT/(double)serviceTime;
+
+            process.setStartTime(startTime);
+            process.setEndTime(endTime);
+            process.setWaitTime(waitTime);
+            process.setTAT(TAT);
+            process.setNormTAT(normTAT);
+
+        }
+
+        return processList;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ import org.xml.sax.SAXException;
 import java.awt.EventQueue;
 
 //import jdk.internal.org.xml.sax.SAXException;
-//test voor git werkt het?
 import java.io.IOException;
 import java.time.Clock;
 import java.util.Collections;
@@ -20,27 +19,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String argv[]) throws ParserConfigurationException, SAXException, IOException {
-        System.out.println("Test voor git....");
         //xmlfiles inlezen
         ReadXMLFile readXMLFile = new ReadXMLFile();
         List<Process> processlist1 = readXMLFile.leesProcessen("processen10000.xml");
-        System.out.println("Lijst met " + processlist1.size() + " processen gecreeerd.");
-
         List<Process> processlist2 = readXMLFile.leesProcessen("processen20000.xml");
-        System.out.println("Lijst met " + processlist2.size() + " processen gecreeerd.");
-
         List<Process> processlist5 = readXMLFile.leesProcessen("processen50000.xml");
-        System.out.println("Lijst met " + processlist5.size() + " processen gecreeerd.");
 
-        List<Process> processlisttest = readXMLFile.leesProcessen("processentmp.xml");
-        System.out.println("Lijst met " + processlisttest.size() + " processen gecreeerd.");
-
-        //sorteer op service time
-        //Collections.sort(processlist1, new SortByServiceTime());
-        //Collections.sort(processlist2, new SortByServiceTime());
-        //Collections.sort(processlist5, new SortByServiceTime());
-        Collections.sort(processlisttest, new SortByServiceTime());
-        
         //FCFS
         List<Process> processlist1FCFS= FirstComeFirstServed.FCFS(processlist1);
         List<Process> processlist2FCFS= FirstComeFirstServed.FCFS(processlist2);
@@ -55,8 +39,6 @@ public class Main {
         
         System.out.println(totgenTAT/processlist1FCFS.size());
         
-        
-        
         //Plotten van grafieken Tw (Time Wait) in functie van Ts (Time Service)
         // en genom. TAT in functie van T sevrice
         plotGenTAT(processlist1FCFS);
@@ -67,6 +49,8 @@ public class Main {
         
         plotGenTAT(processlist5FCFS);
         plotTimeWait(processlist5FCFS);
+
+        //
         
     }
     
